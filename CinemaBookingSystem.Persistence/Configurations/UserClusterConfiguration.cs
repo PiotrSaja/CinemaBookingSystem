7585e43b721a2a2ac2316ee63fe515ad9dd6b384
Piotr Saja <piotreksaja99@gmail.com>
2022-06-12 13:47:30 +0200

    Added user cluster table

---
+++ CinemaBookingSystem.Persistence/Configurations/UserClusterConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CinemaBookingSystem.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CinemaBookingSystem.Persistence.Configurations
+{
+    public class UserClusterConfiguration : IEntityTypeConfiguration<UserCluster>
+    {
+        public void Configure(EntityTypeBuilder<UserCluster> builder)
+        {
+            builder.HasKey(x => x.Id);
+        }
+    }
+}
