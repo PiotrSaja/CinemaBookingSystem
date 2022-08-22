ac7df5cbcd17de2dfa2d6a49af0bec088f8929f6
Piotr Saja <piotreksaja99@gmail.com>
2022-08-22 19:25:47 +0200

    Added recommendations controller with type endpoint

---
+++ CinemaBookingSystem.Application/Recommendations/Queries/WhichRecommendation/WhichRecommendationQuery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CinemaBookingSystem.Domain.Enums;
+using MediatR;
+
+namespace CinemaBookingSystem.Application.Recommendations.Queries.WhichRecommendation
+{
+    public class WhichRecommendationQuery : IRequest<RecommendationType>
+    {
+    }
+}
---
+++ CinemaBookingSystem.Application/Recommendations/Queries/WhichRecommendation/WhichRecommendationQueryHandler.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using CinemaBookingSystem.Application.Common.Interfaces;
+using CinemaBookingSystem.Domain.Enums;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace CinemaBookingSystem.Application.Recommendations.Queries.WhichRecommendation
+{
+    public class WhichRecommendationQueryHandler : IRequestHandler<WhichRecommendationQuery, RecommendationType>
+    {
+        private readonly ICinemaDbContext _context;
+        private readonly IUserService _userService;
+
+        public WhichRecommendationQueryHandler(ICinemaDbContext context, IUserService userService)
+        {
+            _context = context;
+            _userService = userService;
+        }
+
+        public async Task<RecommendationType> Handle(WhichRecommendationQuery request, CancellationToken cancellationToken)
+        {
+            var moviesPref = await _context.UserPreferencesMovies
+                .Where(x => x.UserId == _userService.Id)
+                .Select(x => x.MovieId)
+                .ToListAsync(cancellationToken);
+
+            var moviesVotes = await _context.UserMovieVotes
+                .Where(x => x.UserId == _userService.Id)
+                .ToListAsync(cancellationToken);
+
+            if (moviesPref.Count > 0 && moviesVotes.Count <= 5)
+                return RecommendationType.ContentBased;
+            if (moviesVotes.Count > 5)
+                return RecommendationType.KMeans;
+
+            return RecommendationType.None;
+        }
+    }
+}
