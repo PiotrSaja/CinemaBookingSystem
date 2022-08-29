ac2d4e38265dc7647148299782dc3c68010e2de8
Piotr Saja <piotreksaja99@gmail.com>
2022-08-29 19:37:15 +0200

    Added sending email after created booking

--- CinemaBookingSystem.Api/Controllers/HealthCheckController.cs
+++ CinemaBookingSystem.Api/Controllers/HealthCheckController.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 using CinemaBookingSystem.Application.Common.Interfaces;
 using CinemaBookingSystem.Application.Common.Models;
+using CinemaBookingSystem.Common.Mailer.Abstractions;
+using CinemaBookingSystem.Common.Mailer.Models;
+using CinemaBookingSystem.Common.Mailer.Models.Emails;
 using CinemaBookingSystem.Infrastructure.ExternalAPI.OMDB;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -14,27 +18,12 @@ namespace CinemaBookingSystem.Api.Controllers
     [ApiController]
     public class HealthCheckController : ControllerBase
     {
-        public IOmdbClient OmdbClient;
-
-        public HealthCheckController(IOmdbClient omdbClient)
-        {
-            OmdbClient = omdbClient;
-        }
-
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<string>> GetAsync()
         {
-            var movie = await OmdbClient.GetMovieById("tt2382320", CancellationToken.None);
-
-            MovieModel movieModel =
-                JsonSerializer.Deserialize<MovieModel>(movie);
-
-            Console.WriteLine(movieModel.Title);
-
             return "Api healthy";
         }
-
     }
 }
