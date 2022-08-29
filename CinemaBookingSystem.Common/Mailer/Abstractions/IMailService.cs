d2164097d0cdec24ae1b00bcca0b30725551ff7f
Piotr Saja <piotreksaja99@gmail.com>
2022-08-29 19:06:42 +0200

    Added mailer to sending email via smtp

---
+++ CinemaBookingSystem.Common/Mailer/Abstractions/IMailService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using CinemaBookingSystem.Common.Mailer.Models;
+
+namespace CinemaBookingSystem.Common.Mailer.Abstractions
+{
+    public interface IMailService
+    {
+        Task<bool> SendAsync(MailData mailData, CancellationToken ct);
+        string GetEmailTemplate<T>(string emailTemplate, T emailTemplateModel);
+    }
+}
---
+++ CinemaBookingSystem.Common/Mailer/Models/MailData.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaBookingSystem.Common.Mailer.Models
+{
+    public class MailData
+    {
+        public List<string> To { get; set; }
+        public List<string> Bcc { get; }
+        public List<string> Cc { get; }
+        public string? From { get; }
+        public string? DisplayName { get; set; }
+        public string? ReplyTo { get; set; }
+        public string? ReplyToName { get; set; }
+        public string Subject { get; set; }
+        public string? Body { get; set; }
+        #region MailData()
+        public MailData(List<string> to, string subject, string? body = null, string? from = null, string? displayName = null, string? replyTo = null, string? replyToName = null, List<string>? bcc = null, List<string>? cc = null)
+        {
+            To = to;
+            Bcc = bcc ?? new List<string>();
+            Cc = cc ?? new List<string>();
+            From = from;
+            DisplayName = displayName;
+            ReplyTo = replyTo;
+            ReplyToName = replyToName;
+            Subject = subject;
+            Body = body;
+        }
+
+        public MailData()
+        {
+        }
+        #endregion
+    }
+}
